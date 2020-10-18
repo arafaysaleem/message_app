2198de001ca26762ac9fed2577eb9bcd2e7e5db2
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2020-10-18 19:54:44 +0500

    add documentation to firestore_path.dart

--- lib/services/firestore_path.dart
+++ lib/services/firestore_path.dart
@@ -1,11 +1,25 @@
 //import '../enums/conversation_type_enum.dart';
 
 class FirestorePath{
+
+  /// Path to a collection of conversations of a user.
   static String conversations(String uid) => 'users/$uid/conversations';
-  // static String spammed(String uid) => 'users/$uid/spammed';
-  // static String archived(String uid) => 'users/$uid/archived';
-  static String groups(String uid) => 'users/$uid/groups';
-  //static String conversation(String uid, ConversationType type,String number) => 'users/$uid/${type.getCollectionName()}/$number';
+
+  /// Path to a single conversation of a user, represented by a number.
   static String conversation(String uid,String number) => 'users/$uid/conversations/$number';
+
+  /// Paths for getting the normal/spammed/archived conversation.
+  /// @deprecated after storing all conversations in a single collection.
+  ///
+  /// static String conversation(String uid, ConversationType type,String number) => 'users/$uid/${type.getCollectionName()}/$number';
+  ///
+  /// static String spammed(String uid) => 'users/$uid/spammed';
+  ///
+  /// static String archived(String uid) => 'users/$uid/archived';
+
+  /// Path to a collection of groups of a user.
+  static String groups(String uid) => 'users/$uid/groups';
+
+  /// Path to a single group of a user, represented by a groupID.
   static String group(String uid, String groupID) => 'users/$uid/groups/$groupID';
 }
