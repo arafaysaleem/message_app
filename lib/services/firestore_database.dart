585444150554877ef47728f40963b91ce8e11d60
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2020-10-18 19:11:48 +0500

    add toggle archive convo to firestore_database.dart

--- lib/services/firestore_database.dart
+++ lib/services/firestore_database.dart
@@ -10,75 +10,101 @@ import '../models/conversation.dart';
 String documentIdFromCurrentDate() => DateTime.now().toIso8601String();
 
 class FirestoreDatabase {
-  FirestoreDatabase({@required this.uid})
-      : assert(uid != null, 'Cannot create FirestoreDatabase with null uid');
   final String uid;
-
   final _service = FirestoreService.instance;
 
-  Future<void> addOrUpdateConversation(Conversation conversation) =>
-      _service.setData(
-        path: FirestorePath.conversation(uid, conversation.sender.number),
-        data: conversation.toMap(),
+  FirestoreDatabase({@required this.uid})
+      : assert(uid != null, 'Cannot create FirestoreDatabase with null uid');
+
+  Future<void> addOrUpdateConversation(Conversation conversation) {
+    return _service.setData(
+      path: FirestorePath.conversation(uid, conversation.sender.number),
+      data: conversation.toMap(),
+    );
+  }
+
+  Future<void> readConversation(Conversation conversation) {
+    return _service.documentAction(
+      path: FirestorePath.conversation(uid, conversation.sender.number),
+      changes: {'isRead': conversation.isRead},
+    );
+  }
+
+  Future<void> readGroup(Conversation conversation) {
+    return _service.documentAction(
+      path: FirestorePath.group(uid, conversation.groupID),
+      changes: {'isRead': conversation.isRead},
+    );
+  }
+
+  Future<void> addMessages(Conversation conversation) {
+    return _service.documentAction(
+      path: FirestorePath.conversation(uid, conversation.sender.number),
+      changes: {'messages': conversation.serializeMessages()},
+    );
+  }
+
+  void toggleArchiveSelectedConversations(List<Conversation> _archivedConvos) {
+    _archivedConvos.forEach((convo) {
+      _service.documentAction(
+        path: FirestorePath.conversation(uid, convo.sender.number),
+        changes: {'isArchived': convo.isArchived},
       );
-
-  Future<void> readConversation(Conversation conversation) => _service.documentAction(
-    path: FirestorePath.conversation(uid, conversation.sender.number),
-    changes: {'isRead': conversation.isRead},
-  );
-
-  Future<void> readGroup(Conversation conversation) => _service.documentAction(
-    path: FirestorePath.group(uid, conversation.groupID),
-    changes: {'isRead': conversation.isRead},
-  );
-
-  Future<void> addMessages(Conversation conversation) => _service.documentAction(
-    path: FirestorePath.conversation(uid, conversation.sender.number),
-    changes: {'messages': conversation.serializeMessages()},
-  );
+    });
+  }
 
   void archiveSelectedConversations(List<Conversation> _archivedConvos) =>
       _archivedConvos.forEach(addOrUpdateConversation);
 
-  Future<void> spamSelectedConversation(Conversation _spammedConvo) =>
-      _service.documentAction(
-        path: FirestorePath.conversation(uid, _spammedConvo.sender.number),
-        changes: {'isSpam': _spammedConvo.isSpam},
-      );
+  Future<void> spamSelectedConversation(Conversation _spammedConvo) {
+    return _service.documentAction(
+      path: FirestorePath.conversation(uid, _spammedConvo.sender.number),
+      changes: {'isSpam': _spammedConvo.isSpam},
+    );
+  }
 
-  Future<void> markAllConversationsRead() => _service.batchActon(
-        path: FirestorePath.conversations(uid),
-        changes: {'isRead': true},
-        queryBuilder: (query) => query
-            .where('isSpam', isEqualTo: false)
-            .where('isArchived', isEqualTo: false),
-      );
+  Future<void> markAllConversationsRead() {
+    return _service.batchActon(
+      path: FirestorePath.conversations(uid),
+      changes: {'isRead': true},
+      queryBuilder: (query) => query
+          .where('isSpam', isEqualTo: false)
+          .where('isArchived', isEqualTo: false),
+    );
+  }
 
-  Future<void> deleteConversation(Conversation conversation) =>
-      _service.deleteData(
-        path: FirestorePath.conversation(uid, conversation.sender.number),
-      );
+  Future<void> deleteConversation(Conversation conversation) {
+    return _service.deleteData(
+      path: FirestorePath.conversation(uid, conversation.sender.number),
+    );
+  }
 
-  Future<void> addOrUpdateGroup(Conversation conversation) => _service.setData(
-        path: FirestorePath.group(uid, conversation.groupID),
-        data: conversation.toMap(),
-      );
+  Future<void> addOrUpdateGroup(Conversation conversation) {
+    return _service.setData(
+      path: FirestorePath.group(uid, conversation.groupID),
+      data: conversation.toMap(),
+    );
+  }
 
-  Future<void> deleteGroup(Conversation conversation) => _service.deleteData(
-        path: FirestorePath.group(uid, conversation.groupID),
-      );
+  Future<void> deleteGroup(Conversation conversation) {
+    return _service.deleteData(
+      path: FirestorePath.group(uid, conversation.groupID),
+    );
+  }
 
-  Stream<Conversation> conversationStream({@required String number}) =>
-      _service.documentStream(
-        path: FirestorePath.conversation(uid, number),
-        builder: (data, documentId) => Conversation.fromMap(data),
-      );
+  Stream<Conversation> conversationStream({@required String number}) {
+    return _service.documentStream(
+      path: FirestorePath.conversation(uid, number),
+      builder: (data, documentId) => Conversation.fromMap(data),
+    );
+  }
 
-  Stream<Conversation> groupStream({@required String groupID}) =>
-      _service.documentStream(
-        path: FirestorePath.group(uid, groupID),
-        builder: (data, documentId) => Conversation.fromMap(data),
-      );
+  Stream<Conversation> groupStream({@required String groupID}) {
+    return _service.documentStream(
+      path: FirestorePath.group(uid, groupID),
+      builder: (data, documentId) => Conversation.fromMap(data),
+    );
+  }
 
   /// This method fetches all conversations including spammed and archived
   /// in one list.
@@ -89,37 +115,41 @@ class FirestoreDatabase {
   ///       builder: (data, _) => Conversation.fromMap(data),
   ///     );
 
-  Stream<List<Conversation>> normalStream() =>
-      _service.collectionStream<Conversation>(
-        path: FirestorePath.conversations(uid),
-        queryBuilder: (query) => query
-            .where('isSpam', isEqualTo: false)
-            .where('isArchived', isEqualTo: false),
-        builder: (data, _) => Conversation.fromMap(data),
-        sort: (lhs, rhs) => rhs.sender.name.compareTo(lhs.sender.name),
-      );
+  Stream<List<Conversation>> normalStream() {
+    return _service.collectionStream<Conversation>(
+      path: FirestorePath.conversations(uid),
+      queryBuilder: (query) => query
+          .where('isSpam', isEqualTo: false)
+          .where('isArchived', isEqualTo: false),
+      builder: (data, _) => Conversation.fromMap(data),
+      sort: (lhs, rhs) => rhs.sender.name.compareTo(lhs.sender.name),
+    );
+  }
 
-  Stream<List<Conversation>> spammedStream() =>
-      _service.collectionStream<Conversation>(
-        path: FirestorePath.conversations(uid),
-        queryBuilder: (query) => query.where('isSpam', isEqualTo: true),
-        builder: (data, _) => Conversation.fromMap(data),
-        sort: (lhs, rhs) => rhs.sender.name.compareTo(lhs.sender.name),
-      );
+  Stream<List<Conversation>> spammedStream() {
+    return _service.collectionStream<Conversation>(
+      path: FirestorePath.conversations(uid),
+      queryBuilder: (query) => query.where('isSpam', isEqualTo: true),
+      builder: (data, _) => Conversation.fromMap(data),
+      sort: (lhs, rhs) => rhs.sender.name.compareTo(lhs.sender.name),
+    );
+  }
 
-  Stream<List<Conversation>> archivedStream({Conversation conversation}) =>
-      _service.collectionStream<Conversation>(
-        path: FirestorePath.conversations(uid),
-        queryBuilder: (query) => query.where('isArchived', isEqualTo: true),
-        builder: (data, _) => Conversation.fromMap(data),
-        sort: (lhs, rhs) => rhs.sender.name.compareTo(lhs.sender.name),
-      );
+  Stream<List<Conversation>> archivedStream({Conversation conversation}) {
+    return _service.collectionStream<Conversation>(
+      path: FirestorePath.conversations(uid),
+      queryBuilder: (query) => query.where('isArchived', isEqualTo: true),
+      builder: (data, _) => Conversation.fromMap(data),
+      sort: (lhs, rhs) => rhs.sender.name.compareTo(lhs.sender.name),
+    );
+  }
 
-  Stream<List<Conversation>> groupsStream({Conversation conversation}) =>
-      _service.collectionStream<Conversation>(
-        path: FirestorePath.groups(uid),
-        queryBuilder: (query) => query.where('isGroup', isEqualTo: true),
-        builder: (data, _) => Conversation.fromMap(data),
-        sort: (lhs, rhs) => rhs.groupName.compareTo(lhs.groupName),
-      );
+  Stream<List<Conversation>> groupsStream({Conversation conversation}) {
+    return _service.collectionStream<Conversation>(
+      path: FirestorePath.groups(uid),
+      queryBuilder: (query) => query.where('isGroup', isEqualTo: true),
+      builder: (data, _) => Conversation.fromMap(data),
+      sort: (lhs, rhs) => rhs.groupName.compareTo(lhs.groupName),
+    );
+  }
 }
