79cd7f4ca6671db727381df875a0ee02aca7dc09
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2021-01-30 19:44:11 +0500

    added documentation to contacts_provider.dart
    Signed-off-by: Abdur Rafay Saleem <a.rafaysaleem@gmail.com>

--- lib/providers/contacts_provider.dart
+++ lib/providers/contacts_provider.dart
@@ -6,16 +6,18 @@ import '../services/firestore_database.dart';
 
 import 'messages_provider.dart';
 
-import '../helper/utils.dart';
-
 import '../models/conversation.dart';
 import '../models/contact.dart';
 
 class ContactsProvider with ChangeNotifier {
+
+  /// Instance of firestore database
   FirestoreDatabase _firestoredb = FirestoreDatabase.instance;
 
-  MessagesProvider _messageManager;
+  /// Instance of messages provider
+  MessagesProvider _messagesProvider;
 
+  /// Initializes and loads the contacts for the logged in user
   init(){
     _initializeDefaults();
     _initializeData();
@@ -27,6 +29,7 @@ class ContactsProvider with ChangeNotifier {
   bool _createGroupActive;
   bool _addMemberActive;
 
+  /// Internal method to initialize all variables for the logged in user
   void _initializeDefaults(){
     _contacts = [];
     _selectedContacts = [];
@@ -34,6 +37,7 @@ class ContactsProvider with ChangeNotifier {
     _addMemberActive = false;
   }
 
+  /// Internal method to load all contacts for logged in user
   void _initializeData() {
     _firestoredb.contactsStream().listen((contacts) {
       _contacts = contacts;
@@ -41,14 +45,17 @@ class ContactsProvider with ChangeNotifier {
     });
   }
 
+  /// Method to initialize messages provider for logged in user
   void update(MessagesProvider messageManager) {
-    _messageManager = messageManager;
+    _messagesProvider = messageManager;
   }
 
+  /// Returns an unmodifiable list of all contacts
   UnmodifiableListView<Contact> get contacts => UnmodifiableListView(_contacts);
 
+  /// Returns an unmodifiable list of top 8 contacts
   UnmodifiableListView<Contact> get top8Contacts {
-    final convos = _messageManager.conversations;
+    final convos = _messagesProvider.conversations;
     final List<Contact> last8Convos = [];
     for (int i = convos.length - 1; i > 0; i--) {
       if (convos[i].sender.isAdded) last8Convos.add(convos[i].sender);
@@ -57,63 +64,11 @@ class ContactsProvider with ChangeNotifier {
     return UnmodifiableListView(last8Convos);
   }
 
+  /// Returns an unmodifiable list of selected contacts
   UnmodifiableListView<Contact> get selectedContacts =>
       UnmodifiableListView(_selectedContacts);
 
-  bool isSelected(Contact contact) => _selectedContacts.contains(contact);
-
-  bool get createGroupActive => _createGroupActive;
-
-  bool get addMemberActive => _addMemberActive;
-
-  void toggleCreateGroupMode() {
-    _createGroupActive = !_createGroupActive;
-    notifyListeners();
-  }
-
-  void setAddMemberMode(bool flag) => _addMemberActive = flag;
-
-  void _unSelectContact(Contact contact) {
-    _selectedContacts.remove(contact);
-    notifyListeners();
-  }
-
-  void _selectContact(Contact contact) {
-    _selectedContacts.add(contact);
-    notifyListeners();
-  }
-
-  void toggleSelected(Contact contact) {
-    if (isSelected(contact))
-      _unSelectContact(contact);
-    else
-      _selectContact(contact);
-  }
-
-  void clearSelected() {
-    _selectedContacts.clear();
-    notifyListeners();
-  }
-
-  Conversation createNewGroupConversation(String groupName) {
-    return _messageManager.createGroupConversation(
-        selectedContacts, groupName.isNotEmpty ? groupName : "DEFAULT");
-  }
-
-  void addParticipant(Conversation convo) {
-    convo.addParticipant(_selectedContacts);
-    _firestoredb.addOrUpdateGroup(convo, merge: true);
-    setAddMemberMode(false);
-    clearSelected();
-  }
-
-  Contact getSenderContact(String number) {
-    return _contacts.firstWhere(
-      (Contact contact) => contact.number == number,
-      orElse: () => Contact(number: number, avClr: Utils.getAvatarColor()),
-    );
-  } //call from api helper
-
+  /// Returns an unmodifiable list of filtered contacts matching search text
   UnmodifiableListView<Contact> getFilteredContacts(String text) {
     if (text.isEmpty) {
       return contacts;
@@ -128,6 +83,52 @@ class ContactsProvider with ChangeNotifier {
     return UnmodifiableListView(filteredContacts);
   }
 
+  /// Returns true if a contact is selected
+  bool isSelected(Contact contact) => _selectedContacts.contains(contact);
+
+  /// Returns true if new group is being created
+  bool get createGroupActive => _createGroupActive;
+
+  /// Returns true if a new member is being added
+  bool get addMemberActive => _addMemberActive;
+
+  /// Toggles new group mode
+  void toggleCreateGroupMode() {
+    _createGroupActive = !_createGroupActive;
+    notifyListeners();
+  }
+
+  /// Toggles new member mode
+  void setAddMemberMode(bool flag) => _addMemberActive = flag;
+
+  /// Selects/unselects a contact
+  void toggleSelected(Contact contact) {
+    if (isSelected(contact)) _selectedContacts.remove(contact);
+    else _selectedContacts.add(contact);
+    notifyListeners();
+  }
+
+  /// Unselects all contacts
+  void clearSelected() {
+    _selectedContacts.clear();
+    notifyListeners();
+  }
+
+  /// Creates and returns a new group conversation with the specified name
+  Conversation createNewGroupConversation(String groupName) {
+    return _messagesProvider.createGroupConversation(
+        selectedContacts, groupName.isNotEmpty ? groupName : "DEFAULT");
+  }
+
+  /// Adds a new participant to a conversation and updates the database
+  void addParticipant(Conversation convo) {
+    convo.addParticipant(_selectedContacts);
+    _firestoredb.addOrUpdateGroup(convo, merge: true);
+    setAddMemberMode(false);
+    clearSelected();
+  }
+
+  /// Sets the name for a contact and updates the database
   void addContact(Conversation convo,String name){
     convo.sender.setName(name);
     _contacts.add(convo.sender);
